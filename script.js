9209892fcbf015a64117dd8919a3c89076eb3edf
kasparasd <kasparas.dapsys@gmail.com>
2023-12-22 10:16:11 +0200

    upd

--- script.js
+++ script.js
@@ -1,24 +1,58 @@
-const kmi = document.getElementById("kmiForm");
+const kmiFormSvoris = document.getElementById("kmiFormSvoris");
+const kmiFormUgis = document.getElementById("kmiFormUgis");
 const formDOM = document.getElementById("form");
 const resultKmi = document.getElementById("resultKmi");
+const resetBtn = document.getElementById("resetBtn");
 
 let kmiFromInput = null;
 
 formDOM.addEventListener("submit", (event) => {
   event.preventDefault();
-  kmiFromInput = kmi.value;
-  calculateKmi(kmiFromInput);
+  kmiUgis = kmiFormUgis.value;
+  kmiSvoris = kmiFormSvoris.value;
+
+  resetBtn.classList.remove('reset-btn-display-none')
+  calculateKmi(kmiUgis, kmiSvoris);
+
 });
 
-function calculateKmi(kmiFromInput) {
-  const calculatedKmi = Number(kmiFromInput) * 5;
-  return renderKmiResult(calculatedKmi);
+resetBtn.addEventListener('click', (event)=>{
+    resetBtn.classList.add('reset-btn-display-none')
+    const kmiDomRezultatas = document.getElementById('kmiDomRezultatas')
+    kmiDomRezultatas.remove();
+    formDOM.reset();
+    
+})
+
+function calculateKmi(kmiUgis, kmiSvoris) {
+  const calculatedKmi = ((kmiSvoris / (kmiUgis * kmiUgis)) * 10000).toFixed(2);
+  let text = "";
+  let spalva = "";
+
+  if (calculatedKmi <= 18.5) {
+    spalva = 'yellow'
+    text = `Jus sveriate ${kmiSvoris}, sveriate per mazai, jusu kuno mases indeksas
+    yra: ${calculatedKmi}`;
+  } else if (calculatedKmi > 18.5 && calculatedKmi <= 25) {
+    spalva = 'green'
+    text = `Jus sveriate ${kmiSvoris}, jusu svoris yra normalus, kuno mases indeksas
+    yra: ${calculatedKmi}`;
+  } else if (calculatedKmi > 25 && calculatedKmi <= 30) {
+    spalva = 'orange'
+    text = `Jus sveriate ${kmiSvoris}, sveriate siek tiek per daug, jusu kuno mases indeksas
+    yra: ${calculatedKmi}`;
+  } else if (calculatedKmi > 30) {
+    spalva = 'red'
+    text = `Jus sveriate ${kmiSvoris}, sveriate per daug, jusu kuno mases indeksas
+    yra: ${calculatedKmi}`;
+  }
+  return renderKmiResult(text, spalva);
 }
 
-function renderKmiResult(calculatedKmi) {
-    const text = document.createTextNode("This just got added");
+function renderKmiResult(text, spalva) {
+  // const text = document.createTextNode("This just got added");
 
-    const HTML = `<h2 class="title">${calculatedKmi}</h2>`;
+  const HTML = `<h2 id='kmiDomRezultatas' style="color: ${spalva}" class="title">${text}</h2>`;
 
-    return resultKmi.innerHTML=HTML;
+  return (resultKmi.innerHTML = HTML);
 }
